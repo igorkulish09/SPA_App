31bb274e5992239a0e99210a60003815c2be021b
igor.kulish <igor.kulish09@gmail.com>
2023-12-02 23:36:50 +0200

    test commit

--- SPA/models.py
+++ SPA/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    home_page = models.URLField(blank=True, null=True)
+    captcha = models.CharField(max_length=255)
+    text = models.TextField()
+    parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- core/settings.py
+++ core/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n@f1))5_j(3m^$-l1_!22+1$3ukmth=&7@+6of5m7hs!($vh^h'
-
+SECRET_KEY = ("SECRET_KEY", "django-insecure-n@f1))5_j(3m^$-l1_!22+1$3ukmth=&7@+6of5m7hs!($vh^h")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "SPA",
 ]
 
 MIDDLEWARE = [
